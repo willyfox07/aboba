0eaff1642db750ed74cdf9b766d65e791a0ba8c1
alex <leshastrunec@gmail.com>
2021-11-06 19:06:34 +0200

    changed autentication rools

--- blog/views.py
+++ blog/views.py
@@ -4,23 +4,24 @@ from blog.models import Books
 from django.views import generic
 from blog.forms.Book_form import BookForm, BookCreateForm
 from blog.mixins import RedirectToSuccesUrlMixIn
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class BookView(generic.ListView):
+class BookView(LoginRequiredMixin, generic.ListView):
     """"View for displaying the list of books"""
     queryset = Books.objects.all().order_by('title')
     template_name = 'main_page.html'
     context_object_name = 'Books'
 
 
-class BookDetailView(generic.DetailView):
+class BookDetailView(LoginRequiredMixin, generic.DetailView):
     """"View for displaying the details of a specific book"""
     model = Books
     template_name = 'book_detail.html'
     context_object_name = 'Book'
 
 
-class EditBookDetail(RedirectToSuccesUrlMixIn, generic.UpdateView):
+class EditBookDetail(LoginRequiredMixin, RedirectToSuccesUrlMixIn, generic.UpdateView):
     """"View to edit the details of a specific book"""
     model = Books
     form_class = BookForm
@@ -28,7 +29,7 @@ class EditBookDetail(RedirectToSuccesUrlMixIn, generic.UpdateView):
     slug_url_kwarg = 'slug'
 
 
-class CreateBook(generic.CreateView):
+class CreateBook(LoginRequiredMixin, generic.CreateView):
     """"View to create new book"""
     model = Books
     template_name = 'create_book.html'
@@ -41,7 +42,7 @@ class CreateBook(generic.CreateView):
         return super(CreateBook, self).form_valid(form)
 
 
-class DeleteBook(RedirectToSuccesUrlMixIn,generic.DeleteView):
+class DeleteBook(LoginRequiredMixin, RedirectToSuccesUrlMixIn,generic.DeleteView):
     """View to delete specific book"""
     model = Books
     success_url = reverse_lazy('BookList')
